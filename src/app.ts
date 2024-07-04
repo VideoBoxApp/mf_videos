fd9492b91e404d1b9538bafd885945de680441e9
Daniel Lisboa <daniel.lisbooa@gmail.com>
2024-07-03 22:52:57 -0300

    declarations and app.ts

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import "./global.scss";
+
+function App() {
+  const main = document.createElement("div");
+  return main;
+}
+
+App();
---
+++ src/d.ts
@@ -0,0 +1,2 @@
+declare module "mfDrawer/storeDrawer"
+declare module "mfDrawer/showDrawer"
